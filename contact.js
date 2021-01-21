22c0d89d97d5f3e6a0effe408e261016e80c8bba
34203397 <3420397@petroc.ac.uk>
2021-01-21 16:02:04 +0000

    fix for button

--- contact.js
+++ contact.js
@@ -21,9 +21,9 @@ function topFunction() {
 }
 //----
 //the smooth scroll I am hoping to implement into the web page
-$(document).ready(function(){
+$(document).ready(function()){
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='index.html']").on('click', function(event) {
+  $(".navbar a, footer a[href='index.html']").on('click', function(event)) {
 
    // Make sure this.hash has a value before overriding default behavior
   if (this.hash !== "") {
@@ -36,7 +36,7 @@ $(document).ready(function(){
 
     // Using jQuery's animate() method to add smooth page scroll
     // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
+    $('html, body').animate(){
       scrollTop: $(hash).offset().top
     }, 900, function(){
 
